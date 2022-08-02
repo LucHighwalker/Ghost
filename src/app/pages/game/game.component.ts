d02bbef23b1bc40762f65d893d961ef612e230c8
LucHighwalker <luc.boettner@roomdex.io>
2022-08-02 15:00:52 -0700

    - Getting word list.

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.processWordList();
+  }
+
+  processWordList(): void {
+    this.http.get('assets/WORD.LST', { responseType: 'text' }).subscribe(data => {
+      console.log('data', data.split('\n'))
+    })
   }
 
 }
